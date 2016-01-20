e42d49ddaea9aab2ff6a1e4625c4728adbc08fbb
Ken MacPherson <kmacpher67@yahoo.com>
2016-01-20 12:49:18 -0500

    added sample t2.py jpg to gif

---
+++ t2.py
@@ -0,0 +1,14 @@
+__author__ = 'Robert'
+from images2gif import writeGif
+from PIL import Image
+import os
+
+#ile_names = sorted((fn for fn in os.listdir('.') if fn.endswith('.jpg))
+#['animationframa.png', 'animationframb.png', ...] "
+file_names=['yt1.jpg', 'yt2.jpg', 'yt3.jpg', 'yt4.jpg', 'yt5.jpg', 'yt6.jpg', 'yt7.jpg', 'yt8.jpg', 'yt9.jpg'] 
+
+images = [Image.open(fn) for fn in file_names]
+
+filename = "my_gif.GIF"
+print filename
+writeGif(filename, images, duration=0.5)
