aab614a734510333325b2c6362b7de1485f3d0cc
kmacpher67 <kmacpher67@gmail.com>
2016-01-20 23:42:40 -0500

    camcap and twitup breakout programs

--- t2.py
+++ t2.py
@@ -5,7 +5,8 @@ import os
 
 #ile_names = sorted((fn for fn in os.listdir('.') if fn.endswith('.jpg))
 #['animationframa.png', 'animationframb.png', ...] "
-file_names=['yt1.jpg', 'yt2.jpg', 'yt3.jpg', 'yt4.jpg', 'yt5.jpg', 'yt6.jpg', 'yt7.jpg', 'yt8.jpg', 'yt9.jpg'] 
+file_names=['weather11.jpg', 'weather12.jpg', 'weather20.jpg', 'weather19.jpg', 'weather29.jpg', 'weather7.jpg', 'weather7.jpg', 'weather8.jpg', 'weather9.jpg'] 
+#file_names=['yt1.jpg', 'yt2.jpg', 'yt3.jpg', 'yt4.jpg', 'yt5.jpg', 'yt6.jpg', 'yt7.jpg', 'yt8.jpg', 'yt9.jpg'] 
 
 images = [Image.open(fn) for fn in file_names]
 
