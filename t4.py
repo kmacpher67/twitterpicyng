2f01777e41a74b37df5d17e1551fe88a64747a91
kmacpher67 <kmacpher67@gmail.com>
2016-01-23 07:25:48 -0500

    GIF 5mb limit frames to 19

--- t4.py
+++ t4.py
@@ -3,16 +3,16 @@ from images2gif import writeGif
 from PIL import Image
 import os
 
+imagestoshow=19
 
-imagestoshow=24
 #file_names =  sorted([f for f in os.listdir('.') if f.lower().endswith('.jpg')], key=os.path.getctime, reverse=True)
 file_names =  sorted([f for f in os.listdir('.') if f.lower().endswith('.jpg')], key=os.path.getctime)
 if len(file_names)>imagestoshow:
-	file_names=file_names[len(file_names)-imagestoshow-5:len(file_names)-5]
+	file_names=file_names[len(file_names)-imagestoshow:len(file_names)]
 print file_names
 
 images = [Image.open(fn) for fn in file_names]
 
 filename = "my_gif.GIF"
 print filename
-writeGif(filename, images, duration=0.35)
+writeGif(filename, images, duration=0.4)
