a100c6d6d1c47301ef2fdf985f0ac4cdba6ed1e6
kmacpher67 <kmacpher67@gmail.com>
2016-04-24 19:37:30 -0400

    edit time on gif maker to 1/2 sec

--- t4.py
+++ t4.py
@@ -3,7 +3,7 @@ from images2gif import writeGif
 from PIL import Image
 import os
 
-imagestoshow=20
+imagestoshow=21
 
 #file_names =  sorted([f for f in os.listdir('.') if f.lower().endswith('.jpg')], key=os.path.getctime, reverse=True)
 file_names =  sorted([f for f in os.listdir('.') if f.lower().endswith('.jpg')], key=os.path.getctime)
@@ -15,4 +15,4 @@ images = [Image.open(fn) for fn in file_names]
 
 filename = "my_gif.GIF"
 print filename
-writeGif(filename, images, duration=0.4)
+writeGif(filename, images, duration=0.5)
